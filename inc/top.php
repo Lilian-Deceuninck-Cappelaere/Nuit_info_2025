7149fa9c2990972c682434364bef44f888676d45
lilian <deceuninck.lilian1@gmail.com>
2025-12-05 05:22:25 +0100

    ajout texte présentation

--- inc/top.php
+++ inc/top.php
@@ -3,7 +3,10 @@
     <h2>Présentation</h2>
 
     <p class="presentation-text">
-        Bienvenue sur la plateforme NIRD. Découvrez notre démarche pour un numérique plus inclusif et responsable grâce à nos outils interactifs.
+        Bienvenue sur notre site, nous allons vous présenter la Démarche NIRD : pour un numérique libre et écocitoyen dans les établissements scolaires !
+        Un collectif enseignant invite les établissements scolaires et les collectivités qui les accompagnent à s’engager progressivement vers un Numérique qui soit davantage Inclusif, Responsable et Durable, en rejoignant la « démarche NIRD  ».
+        Elle se repose sur trois piliers l’inclusion qui est un accès équitable au numérique et une réduction de la fracture numérique, la responsabilité qui est un usage raisonné et réflexif de technologies souveraines et respectueuses des données personnelles et la durabilité qui lutte contre l’obsolescence programmée par le choix de Linux pour l’équipement, maîtrise des coûts…
+        Pour plus d’info aller sur <a href="https://nird.forge.apps.education.fr/index.html">nird.forge.apps.education.fr</a>
     </p>
 
     <div class="image-container">
